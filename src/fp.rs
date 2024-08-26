ff7f1627f171a668ec2acbbcbe3d9b1bdfb633a8
iquerejeta <querejeta.azurmendi@iohk.io>
2024-08-26 13:18:17 +0200

    Add MODULUS to Fp

--- src/fp.rs
+++ src/fp.rs
@@ -766,8 +766,8 @@ impl PrimeField for Fp {
         todo!()
     }
 
-    const MODULUS: &'static str = "";
-    const NUM_BITS: u32 = 0;
+    const MODULUS: &'static str = "0x1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaaab";
+    const NUM_BITS: u32 = 32;
     const CAPACITY: u32 = 0;
     const TWO_INV: Self = TWO_INV;
     const MULTIPLICATIVE_GENERATOR: Self = MULTIPLICATIVE_GENERATOR;
